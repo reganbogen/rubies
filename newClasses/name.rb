ed1b312b1a5e16601fc0cad6ec8888a5ce63bb38
Regan Layman <regan.layman@gmail.com>
2020-08-25 15:06:46 -0400

    added in method to create full_name and then called it at the end

--- newClasses/name.rb
+++ newClasses/name.rb
@@ -1,6 +1,5 @@
 class Name
-  attr_reader :first_name, :middle_name, :last_name
-  attr_accessor :title
+  attr_accessor :title, :first_name, :middle_name, :last_name
   
   
   def initialize(title, first_name, middle_name, last_name)
@@ -10,10 +9,12 @@ class Name
     @last_name =last_name
   end
   
+  def full_name
+    @first_name + " " + @middle_name + " " + @last_name
+  end
+  
+  
 end
 
 name = Name.new("Mrs.", "Juniper", "Louise", "Layman")
-puts name.title + " " +
-  name.first_name + " " +
-  name.middle_name + " " +
-  name.last_name
+puts name.title + " " + name.full_name
