10fcc91008d1a3eb663c3203e0284d1ce87daffe
Regan Layman <regan.layman@gmail.com>
2020-08-25 19:32:37 -0400

    added attr_reader for name and an add_transaction method that i now realize I spelled wrong, and will correct

--- newClasses/bank_account.rb
+++ newClasses/bank_account.rb
@@ -1,10 +1,19 @@
 class BankAccount 
+  attr_reader :name 
+  
   def initialize(name)
     @name = name
     @transactions = []
+  end
+  
+  def add_transation(description, amount)
+    @transactions.push(description: description, amount: amount)
   end
 end
 
 bank_account = BankAccount.new("Toggle")
 puts bank_account.inspect
+bank_account.add_transaction("Groceries", 40)
+puts bank_account.inspect
+
 
